30e55f79a2d97dd01b352b3eab6c7668ca688f03
Alok Chauhan <alokchauhan589@gmail.com>
2021-08-01 04:15:28 +0000

    latest commit

--- inference.py
+++ inference.py
@@ -35,14 +35,79 @@ from piqa import ssim
 from skimage.metrics import structural_similarity as ssim
 from Deep_Res_SE_Unet import Deep_Res_SE_Unet
 
+##################
+from skimage import transform as tf
+import torchvision
+from skimage import transform
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from cv2 import exp, transform
+from numpy.lib.npyio import save
+import dataset
+import matplotlib.pyplot as plt
+import torch
+import scipy.io as sio
+import numpy as np
+import os
+import skimage.io
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from skimage import io
 
-# class SSIMLoss(ssim.SSIM):
-#     def forward(self, x, y):
-#         return 1.0 - super().forward(x, y)
+# from torch.utils.tensorboard import SummaryWriter
+from torch.optim import optimizer
+
+# from Deep_Res_Unet import Deep_Res_Unet
+from engine import evaluate
+import cv2
+from pytorch_msssim import ssim, ms_ssim
+import pytorch_ssim
+from piqa import ssim
+from skimage.metrics import structural_similarity as ssim
+
+# engine.py
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torch._C import device
+from torch.optim import optimizer
+import numpy as np
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import pytorch_ssim
+from pytorch_msssim import ssim, ms_ssim
+from piqa import ssim
+from torch.autograd.grad_mode import F
+from collections import namedtuple
+import torch
+from torchvision import models
+from torchvision.models.vgg import vgg16
+from datetime import datetime
+import datetime
+from Deep_Res_SE_Unet import Deep_Res_SE_Unet
+
+# from test_3 import test_loader
+import pandas as pd
+from Res_Unet import Res_Unet
+from new_model import new_model
 
 
-model = Deep_Res_SE_Unet()
-model.load_state_dict(torch.load("/home/thesis_2/model_opt_chp/RSEUnet.pt")["model"])
+class SSIMLoss(ssim.SSIM):
+    def forward(self, x, y):
+        return 1.0 - super().forward(x, y)
+
+
+# model = Deep_Res_SE_Unet()
+model = new_model()
+model.load_state_dict(
+    torch.load("/home/thesis_2/model_opt_chp/transfer_reduced_depth.pt")["model"]
+)
 model.eval()
 device = "cuda:5"
 
@@ -50,11 +115,19 @@ device = "cuda:5"
 #     "/home/thesis_bk/dataset/measurements/n01440764/n01440764_457..png"
 # )
 
-image_idx = 5020  # 12564  # 2126  # 5020  # 50565  # 1086#9022 #99 #1039 #25 #5020
-x = np.load("/home/thesis_2/mnist_dataset/mnist_measures.npy")[image_idx]
-y = np.load("/home/thesis_2/mnist_dataset/mnist_imgs.npy")[image_idx]
+image_idx = (
+    1039  # 52156  # 12564  # 2126  # 5020  # 50565  # 1086#9022 #99 #1039 #25 #5020
+)
+x = np.load("/home/thesis_2/Emnist_dataset/emnist_measures.npy")[image_idx]
+
+mu, sigma = 0, 0.02  # mean and standard deviation
+s = np.random.normal(mu, sigma, [128, 128])
+x = x + s
+x = torch.tensor(x).to(torch.float32)
+
+y = np.load("/home/thesis_2/Emnist_dataset/emnist_imgs.npy")[image_idx]
 y = cv2.resize(y, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
-z = np.load("/home/thesis_2/mnist_dataset/mnist_measures.npy")[image_idx]
+z = np.load("/home/thesis_2/Emnist_dataset/emnist_measures.npy")[image_idx]
 
 
 x = np.expand_dims(x, 0)
@@ -79,26 +152,33 @@ plt.suptitle(f"ssim: {ssim.item()}, psnr:{psnr.item()}")
 
 plt.subplot(1, 4, 1)
 plt.imshow(z)
-plt.title("measurements")
+plt.axis("off")
+
+# plt.title("measurements")
 
 plt.subplot(1, 4, 2)
 pred1 = pred[0][0].cpu().detach().numpy()
 plt.imshow(pred1)
-plt.title("pred")
+plt.axis("off")
+
+# plt.title("pred")
 
 y1 = y[0][0].cpu().detach().numpy()
 plt.subplot(1, 4, 3)
 plt.imshow(y1)
-plt.title("real_image")
+plt.axis("off")
+
+# plt.title("real_image")
 
 plt.subplot(1, 4, 4)
 plt.imshow(np.abs(pred1 - y1))
-plt.title("Pred - real_image")
+plt.axis("off")
+# plt.title("Pred - real_image")
 
 # plot_name = f"{idx}.png"
 
-plot_name = "1" + ".png"
-plt.savefig(os.path.join("/home/thesis_2/new_inf", plot_name))
+plot_name = "transfer_reduced_depth" + ".png"
+plt.savefig(os.path.join("/home/thesis_2/inference_plots", plot_name))
 
 # print("recon", recn)
 # print(recn.shape)
